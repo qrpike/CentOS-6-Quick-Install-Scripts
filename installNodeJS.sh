fde0022b23479f8470ca97fab6140e23a5caeb98
Anthony Webb <anthony.webb@gmail.com>
2013-02-11 22:49:41 -0700

    Update installNodeJS.sh

--- installNodeJS.sh
+++ installNodeJS.sh
@@ -3,7 +3,7 @@
 # CHANGE THIS URL FOR DIFF VERSIONS :::
 # -----------------------------------------------------
 
-JSURL=http://nodejs.org/dist/node-latest.tar.gz
+JSURL=http://nodejs.org/dist/v0.8.19/node-v0.8.19.tar.gz
 
 # -----------------------------------------------------
 

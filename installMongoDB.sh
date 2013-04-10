87f68ac4ec45e6efc928c59929edda4881059688
mani <mani95lisa@gmail.com>
2013-04-10 22:04:58 +0800

    change mongo and nodejs

--- installMongoDB.sh
+++ installMongoDB.sh
@@ -11,7 +11,22 @@ echo 'enabled=1' >> /etc/yum.repos.d/10gen.repo
 
 yum install -y mongo-10gen mongo-10gen-server
 
-service mongod start
+echo 'Begin to config MongoDB...'
+read -p 'Enter path to save config : (/etc/mongod.conf) ' config
+config=${config:-/etc/mongod.conf}
+touch $config
+read -p 'Enter path to save data : (/data/db) ' dbpath
+dbpath=${dbpath:-/data/db}
+echo 'dbpath = '$dbpath >> $config
+read -p 'Enter path to save log : (/var/log/mongodb/mongod.log) ' logpath
+logpath=${logpath:-/var/log/mongodb/mongod.log}
+echo 'logpath = '$logpath >> $config
+echo 'fork = true' >> $config
+echo 'port = 27017' >> $config
+echo 'bind_ip = 127.0.0.1' >> $config
+echo 'logappend = true' >> $config
+
+service mongod --config $config
 chkconfig mongod on
 clear
 echo 'MongoDB is installed, running and set to auto-start on reboots.... your still a NoSQL groupie...'
--- installNodeJS.sh
+++ installNodeJS.sh
@@ -1,45 +1,10 @@
 #!/bin/bash
-
-#install NTP just incase:
-source <(curl -s https://raw.github.com/qrpike/CentOS-6-Quick-Install-Scripts/master/installNTP.sh --insecure)
-
-# CHANGE THIS URL FOR DIFF VERSIONS :::
-# -----------------------------------------------------
-
-JSURL=http://nodejs.org/dist/v0.10.0/node-v0.10.0-linux-x64.tar.gz
-
-# -----------------------------------------------------
-
-FILENAME=${JSURL##*/} # get the filename from the URL
-TEMPFOLDER="${FILENAME%.*}"
-FOLDER="${TEMPFOLDER%.*}"
-
-clear
-
-echo 'We are going to install NodeJS for you... '
-
-echo 'checking if we got wget magic'
-yum install -y wget #check if we have it
-
-echo 'lets download nodejs..'
-
-rm -r -f /usr/local/src
-mkdir -p /usr/local/src
-cd /usr/local/src
-
-wget $JSURL
-
-mkdir -p ./nodesrc
-tar -zxvf ./$FILENAME
-
-echo 'Files extracted....'
-
-cd ./$FOLDER
-
-yum install openssl-devel gcc-c++ gcc
-yum groupinstall -y "Development Tools"
-
-echo 'export PATH=/usr/local/src/node-v0.10.0-linux-x64/bin:$PATH' >> /etc/profile
-source /etc/profile
+yum install git -y
+git clone git://github.com/creationix/nvm.git ~/nvm
+echo "source ~/nvm/nvm.sh" >> ~/.bashrc
+source ~/.bashrc 
+read -p 'Enter node.js version to install : (0.10.2) ' version
+version=${version:-0.10.2}
+nvm install version
 
 echo 'welllllll....... that should be it.... hopefully :)'

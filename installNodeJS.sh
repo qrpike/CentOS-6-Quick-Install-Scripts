7eaa44f3db4f9c4986e0e99813d2f6e3c5b0ff50
Quinton Pike <qrpike@gmail.com>
2013-05-26 21:29:55 -0400

    Update installNodeJS.sh

--- installNodeJS.sh
+++ installNodeJS.sh
@@ -10,8 +10,9 @@ yum install -y wget
 
 wget http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
 wget http://rpms.famillecollet.com/enterprise/remi-release-6.rpm
-sudo rpm -Uvh remi-release-6*.rpm epel-release-6*.rpm
+rpm -Uvh remi-release-6*.rpm epel-release-6*.rpm
 
+ls -1 /etc/yum.repos.d/epel* /etc/yum.repos.d/remi.repo
 
 yum install -y nodejs npm
 

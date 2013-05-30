a0dbf544f4e1f554a172ce66d606eb9201698045
Quinton Pike <qrpike@gmail.com>
2013-05-30 16:11:44 -0300

    Updated to compile and use latest version

--- installNodeJS.sh
+++ installNodeJS.sh
@@ -1,22 +1,55 @@
 #!/bin/bash
 
-cd ~/
-
 # install NTP just incase:
 # - NodeJS throws an error if you try to compile with a server date before the NodeJS release date
 source <(curl -s https://raw.github.com/qrpike/CentOS-6-Quick-Install-Scripts/master/installNTP.sh --insecure)
 
-yum install -y wget
+# CHANGE THIS URL FOR DIFF VERSIONS :::
+# -----------------------------------------------------
 
-wget http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
-wget http://rpms.famillecollet.com/enterprise/remi-release-6.rpm
-rpm -Uvh remi-release-6*.rpm epel-release-6*.rpm
+JSURL=http://nodejs.org/dist/node-latest.tar.gz
 
-ls -1 /etc/yum.repos.d/epel* /etc/yum.repos.d/remi.repo
+# -----------------------------------------------------
 
-yum install -y nodejs npm
+FILENAME=${JSURL##*/} # get the filename from the URL
+TEMPFOLDER="${FILENAME%.*}"
+FOLDER="${TEMPFOLDER%.*}"
+
+clear
+
+echo 'We are going to install NodeJS for you... '
+
+echo 'checking if we got wget magic'
+yum install -y wget #check if we have it
+
+echo 'lets download nodejs..'
+
+rm -r -f /usr/local/src
+mkdir -p /usr/local/src
+cd /usr/local/src
+
+wget $JSURL
+
+mkdir -p ./nodesrc
+tar -zxvf ./$FILENAME
+
+echo 'Files extracted....'
+
+cd ./$FOLDER
+
+yum install -y openssl-devel
+yum groupinstall -y "Development Tools"
+
+echo 'Configuring and installing NodeJS'
+
+./configure
+make
+make install
+
+echo 'welllllll....... that should be it.... hopefully :)'
 
 clear
 
 echo "Node is now installed @ version:"
 node --version
+

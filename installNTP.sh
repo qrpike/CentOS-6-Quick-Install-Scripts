038a18a00c5fcd78806aa86dcd3234b5cdfb6c37
Quinton Pike <qrpike@gmail.com>
2013-06-21 02:03:12 -0300

    Update installNTP.sh

--- installNTP.sh
+++ installNTP.sh
@@ -7,6 +7,12 @@ chkconfig ntpd on
 ntpdate pool.ntp.org
 /etc/init.d/ntpd start
 
+
+# Set timezone
+rm -f /etc/localtime
+ln -s /usr/share/zoneinfo/America/New_York /etc/localtime
+
+
 echo 'NTP is setup and configured'
 echo 'Below should be the correct date:'
 date

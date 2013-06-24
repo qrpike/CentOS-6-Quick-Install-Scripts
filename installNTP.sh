cdca5dfeb789b5d7849d8602158f8e22a6937730
Quinton Pike <qrpike@gmail.com>
2013-06-24 16:33:39 -0300

    Update installNTP.sh

--- installNTP.sh
+++ installNTP.sh
@@ -17,8 +17,11 @@ echo 'NTP is setup and configured'
 echo 'Below should be the correct date:'
 date
 
-
+# Replace centos pool with regular NTP pool
 sed -i 's/centos.pool.ntp.org/pool.ntp.org iburst/g' /etc/ntp.conf
+
+# Add this to not fail on big time gaps ( VMs that resume/pause )
+echo "tinker panic 0" >> /etc/ntp.conf 
 
 # Add a time message on SSH logins:
 cat >>~/.bashrc <<EOL

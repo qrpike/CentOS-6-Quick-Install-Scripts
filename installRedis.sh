6c73ef9372f3e58bb0ce1b68460498ba4faf28a7
Quinton Pike <qrpike@gmail.com>
2013-12-05 00:05:58 -0500

    Create installRedis.sh

---
+++ installRedis.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+yum install -y wget
+
+clear
+echo "Installing Redis: STABLE"
+
+wget http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
+wget http://rpms.famillecollet.com/enterprise/remi-release-6.rpm
+rpm -Uvh remi-release-6*.rpm epel-release-6*.rpm
+yum install redis -y
+
+echo "All Done..."
